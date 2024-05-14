5e5ca593a90719b51d9dfc1750f1caca868cc030
dave17code <dave17code@gmail.com>
2024-05-15 01:16:51 +0900

    240515HomeFirst

--- AppStoreBibleTap/BibleTapVC.swift
+++ AppStoreBibleTap/BibleTapVC.swift
@@ -10,12 +10,30 @@ import UIKit
 class BibleTapVC: UIViewController {
     
     @IBOutlet weak var bibleVerseContainerView: UIView!
+    @IBOutlet var bibleVerseThemeButton: [UIButton]!
+    @IBOutlet weak var bibleVerseShareButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bibleVerseContainerView.layer.borderWidth = 1.6
         bibleVerseContainerView.layer.cornerRadius = 12
+        bibleVerseContainerView.layer.borderWidth = 1.6
         addSwipeGesture()
+        for button in bibleVerseThemeButton {
+            button.layer.cornerRadius = 12
+            button.layer.borderWidth = 1.2
+        }
+        bibleVerseShareButton.layer.cornerRadius = 12
+        bibleVerseShareButton.layer.borderWidth = 1.2
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if UserDefaults.standard.object(forKey: "selectedButtonIndex") == nil {
+            updateButtonSelection(selectedButtonIndex: 0)
+        } else {
+            let selectedButtonIndex = UserDefaults.standard.integer(forKey: "selectedButtonIndex")
+            updateButtonSelection(selectedButtonIndex: selectedButtonIndex)
+        }
     }
     
     func addSwipeGesture() {
@@ -28,5 +46,30 @@ class BibleTapVC: UIViewController {
         if let settingVC = storyboard?.instantiateViewController(withIdentifier: "SettingVC") as? SettingVC {
             navigationController?.pushViewController(settingVC, animated: true)
         }
+    }
+    
+    @IBAction func bibleVerseThemeButton(_ sender: UIButton) {
+        if let index = bibleVerseThemeButton.firstIndex(of: sender) {
+            updateButtonSelection(selectedButtonIndex: index)
+        }
+    }
+    
+    private func updateButtonSelection(selectedButtonIndex: Int) {
+        UserDefaults.standard.set(selectedButtonIndex, forKey: "selectedButtonIndex")
+        UIView.animate(withDuration: 0.2, animations: {
+            for (index, button) in self.bibleVerseThemeButton.enumerated() {
+                if index == selectedButtonIndex {
+                    button.isSelected = true
+                    button.backgroundColor = .systemBlue
+                    button.layer.borderWidth = 0
+                    button.setTitleColor(.white, for: .normal)
+                } else {
+                    button.isSelected = false
+                    button.backgroundColor = .white
+                    button.layer.borderWidth = 1.2
+                    button.setTitleColor(.black, for: .normal)
+                }
+            }
+        })
     }
 }
