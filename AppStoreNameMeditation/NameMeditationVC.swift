16687626bd0ed2252b5bcec620bf350f01f39f57
dave17code <dave17code@gmail.com>
2024-05-20 21:17:45 +0900

    240520CafeSecond

--- AppStoreNameMeditation/NameMeditationVC.swift
+++ AppStoreNameMeditation/NameMeditationVC.swift
@@ -10,15 +10,38 @@ import UIKit
 class NameMeditationVC: UIViewController {
     
     @IBOutlet weak var bibleVerseContainerView: UIView!
-    @IBOutlet weak var bibleVerseShareButton: UIButton!
+    @IBOutlet weak var bibleVerseVStackView: UIStackView!
     @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var bibleVerseShareButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.layer.cornerRadius = 12
         nameTextField.layer.borderWidth = 1.2
-        bibleVerseContainerView.layer.cornerRadius = 12
-        bibleVerseContainerView.layer.borderWidth = 1.6
+//        bibleVerseContainerView.layer.cornerRadius = 12
+//        bibleVerseContainerView.layer.borderWidth = 1.6
         bibleVerseShareButton.layer.cornerRadius = 12
+    }
+    
+    @IBAction func meditateButton(_ sender: Any) {
+        let rectView = UIView()
+        rectView.translatesAutoresizingMaskIntoConstraints = false
+        rectView.layer.borderWidth = 1.6
+        rectView.layer.cornerRadius = 12
+        bibleVerseContainerView.addSubview(rectView)
+        let stackViewHeight = bibleVerseVStackView.frame.height
+        NSLayoutConstraint.activate([
+            rectView.leadingAnchor.constraint(equalTo: bibleVerseContainerView.leadingAnchor),
+            rectView.trailingAnchor.constraint(equalTo: bibleVerseContainerView.trailingAnchor),
+            rectView.centerXAnchor.constraint(equalTo: bibleVerseContainerView.centerXAnchor),
+            rectView.centerYAnchor.constraint(equalTo: bibleVerseVStackView.centerYAnchor),
+            rectView.heightAnchor.constraint(equalToConstant: stackViewHeight + 100)
+        ])
+        rectView.alpha = 0
+        UIView.animate(withDuration: 0.5) {
+            rectView.alpha = 1
+        }
     }
 }
