2ceb66a0ee25276612748e02d54f4867533eb7ff
dave17code <dave17code@gmail.com>
2024-05-21 17:37:00 +0900

    240521HomeSecond

--- AppStoreNameMeditation/Model.swift
+++ AppStoreNameMeditation/Model.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct BibleVerseModel {
+    private init() {}
+    static var shared = BibleVerseModel()
+    var userName: String = ""
+    
+    let bibleVerseDictionary: [[String: String]] = [
+        ["시편 23:1-3": "여호와는 name의 목자시니 name이 부족함이 없으리로다. 그가 name을 푸른 초장에 누이시며 쉴 만한 물가로 인도하시는도다. name의 영혼을 소생시키시고 자기 이름을 위하여 의의 길로 인도하시는도다."],
+        ["이사야 40:31": "오직 여호와를 앙망하는 name은 새 힘을 얻으리니 독수리가 날개치며 올라감 같을 것이요. 달음박질하여도 곤비치 아니하겠고, 걸어가도 피곤치 아니하리로다."]
+    ]
+    
+    func getBibleVerse(_ index: Int, _ key: String) -> String? {
+        guard let template = bibleVerseDictionary[index][key] else {
+            return nil
+        }
+        return template.replacingOccurrences(of: "name", with: userName)
+    }
+}
--- AppStoreNameMeditation/NameMeditationVC.swift
+++ AppStoreNameMeditation/NameMeditationVC.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
-import Photos
 
 class NameMeditationVC: UIViewController {
     
+    var selectedVerseKey: String = "시편 23:1-3" // 선택된 성경 구절 키
+        var selectedVerseIndex: Int = 0 // 선택된 성경 구절 인덱스
+    
     @IBOutlet weak var bibleVerseContainerView: UIView!
     @IBOutlet weak var bibleVerseVStackView: UIStackView!
+    @IBOutlet weak var bibleVerseLabel: UILabel!
+    @IBOutlet weak var bibleVerseChapterLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var bibleVerseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,53 +25,55 @@ class NameMeditationVC: UIViewController {
         nameTextField.layer.cornerRadius = 12
         bibleVerseContainerView.layer.borderWidth = 1.6
         bibleVerseContainerView.layer.cornerRadius = 12
+        setUpBibleVersePopupMenu()
     }
     
     @IBAction func meditateButton(_ sender: Any) {
+        if let userName = nameTextField.text {
+            BibleVerseModel.shared.userName = userName
+            if let verse = BibleVerseModel.shared.getBibleVerse(selectedVerseIndex, "\(bibleVerseButton.titleLabel?.text ?? "")") {
+                bibleVerseLabel.setTextWithFadeAnimation(verse, duration: 1.0)
+            }
+        }
     }
     
-    @IBAction func captureButton(_ sender: Any) {
-        takeScreenshotAndSave()
-    }
+    func setUpBibleVersePopupMenu() {
+          let psalmAction = UIAction(title: "시편 23:1-3", state: selectedVerseKey == "시편 23:1-3" ? .on : .off) { action in
+              self.selectedVerseKey = "시편 23:1-3"
+              self.selectedVerseIndex = 0
+              self.updateBibleVerseButtonTitle()
+              self.setUpBibleVersePopupMenu() // 메뉴를 다시 설정하여 체크 표시 업데이트
+          }
+          
+          let isaiahAction = UIAction(title: "이사야 40:31", state: selectedVerseKey == "이사야 40:31" ? .on : .off) { action in
+              self.selectedVerseKey = "이사야 40:31"
+              self.selectedVerseIndex = 1
+              self.updateBibleVerseButtonTitle()
+              self.setUpBibleVersePopupMenu() // 메뉴를 다시 설정하여 체크 표시 업데이트
+          }
+          
+          let menu = UIMenu(title: "성경 구절 선택", options: .displayInline, children: [psalmAction, isaiahAction])
+          bibleVerseButton.menu = menu
+          bibleVerseButton.showsMenuAsPrimaryAction = true
+      }
     
-    //MARK: Functions
-    func takeScreenshotAndSave() {
-        
-        guard let view = UIApplication.shared.windows.first?.rootViewController?.view else {
-            return
-        }
-        
-        // Create an image renderer
-        let renderer = UIGraphicsImageRenderer(size: view.bounds.size)
-        
-        // Render the view into an image
-        let screenshotImage = renderer.image { context in
-            view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
-        }
-        
-        // Save the screenshot to the Photos library
-        UIImageWriteToSavedPhotosAlbum(screenshotImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    func updateBibleVerseButtonTitle() {
+        let customFont = UIFont(name: "BMYEONSUNG-OTF", size: 17) ?? UIFont.systemFont(ofSize: 17)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: customFont,
+            .foregroundColor: UIColor.white
+        ]
+        let attributedTitle = NSAttributedString(string: selectedVerseKey, attributes: attributes)
+        bibleVerseButton.setAttributedTitle(attributedTitle, for: .normal)
     }
 }
 
-extension NameMeditationVC {
-    
-    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeMutableRawPointer?) {
-        if let error = error {
-            // Handle error saving the image
-            showToast(message: "Error saving image: \(error.localizedDescription)")
-        } else {
-            // Image saved successfully
-            showToast(message: "ScreenShot Taken")
+extension UILabel {
+    func setTextWithFadeAnimation(_ text: String, duration: TimeInterval) {
+        self.alpha = 0.0
+        self.text = text
+        UIView.animate(withDuration: duration) {
+            self.alpha = 1.0
         }
-    }
-    
-    func showToast(message: String) {
-        let toast = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        UIApplication.shared.keyWindow?.rootViewController?.present(toast, animated: true, completion: {
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
-                toast.dismiss(animated: true)
-            }
-        })
     }
 }
