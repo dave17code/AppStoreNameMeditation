49acf5d00da23f9f8630c398264a82a502daede1
dave17code <dave17code@gmail.com>
2024-06-03 22:04:54 +0900

    240603Fifth

--- AppStoreNameMeditation/Model.swift
+++ AppStoreNameMeditation/Model.swift
@@ -13,15 +13,19 @@ struct Model {
     var userName: String = ""
     
     let nameInBibleVerseDictionary: [[String: String]] = [
-        ["시편 23:1-3": "여호와는 name의 목자시니 name이 부족함이 없으리로다. 그가 name을 푸른 초장에 누이시며 쉴만한 물가로 인도하시는도다. name의 영혼을 소생시키시고 자기 이름을 위하여 의의 길로 인도하시는도다."],
-        ["이사야 40:31": "오직 여호와를 앙망하는 name은 새 힘을 얻으리니 독수리가 날개치며 올라감 같을 것이요. 달음박질하여도 곤비치 아니하겠고, 걸어가도 피곤치 아니하리로다."],
-        ["이사야 41:10": "두려워하지 말라, 내가 name과 함께 함이니라. 놀라지 말라, 나는 name의 하나님이 됨이니라. 내가 name을 굳세게 하리라. 참으로 name을 도와 주리라. 참으로 나의 의로운 오른손으로 name을 붙들리라."]
+        ["신명기 31:6": "name은 강하고 담대하라 두려워하지 말라 그들 앞에서 떨지 말라 이는 네 하나님 여호와 그가 name과 함께 행하시며 결코 name을 떠나지 아니하시며 버리지 아니하실 것임이니라"
+        ],
+        ["시편 23:1-3": "여호와는 name의 목자시니 name이 부족함이 없으리로다 그가 name을 푸른 초장에 누이시며 쉴만한 물가로 인도하시는도다 name의 영혼을 소생시키시고 자기 이름을 위하여 의의 길로 인도하시는도다"],
+        ["이사야 40:31": "오직 여호와를 앙망하는 name은 새 힘을 얻으리니 독수리가 날개치며 올라감 같을 것이요 달음박질하여도 곤비치 아니하겠고 걸어가도 피곤치 아니하리로다"],
+        ["이사야 41:10": "두려워하지 말라 내가 name과 함께 함이니라 놀라지 말라 나는 name의 하나님이 됨이니라 내가 name을 굳세게 하리라 참으로 name을 도와 주리라 참으로 나의 의로운 오른손으로 name을 붙들리라"]
     ]
     
     let originalBibleVerseDictionary: [[String: String]] = [
-        ["시편 23:1-3": "여호와는 나의 목자시니 내가 부족함이 없으리로다. 그가 나를 푸른 초장에 누이시며 쉴만한 물가로 인도하시는도다. 내 영혼을 소생시키시고 자기 이름을 위하여 의의 길로 인도하시는도다."],
-        ["이사야 40:31": "오직 여호와를 앙망하는 자는 새 힘을 얻으리니 독수리가 날개치며 올라감 같을 것이요. 달음박질하여도 곤비치 아니하겠고, 걸어가도 피곤치 아니하리로다."],
-        ["이사야 41:10": "두려워하지 말라, 내가 너와 함께 함이니라. 놀라지 말라, 나는 네 하나님이 됨이니라. 내가 너를 굳세게 하리라. 참으로 너를 도와 주리라. 참으로 나의 의로운 오른손으로 너를 붙들리라."]
+        ["신명기 31:6": "너희는 강하고 담대하라 두려워하지 말라 그들 앞에서 떨지 말라 이는 네 하나님 여호와 그가 너와 함께 행하시며 결코 너를 떠나지 아니하시며 버리지 아니하실 것임이니라"
+        ],
+        ["시편 23:1-3": "여호와는 나의 목자시니 내가 부족함이 없으리로다 그가 나를 푸른 초장에 누이시며 쉴만한 물가로 인도하시는도다 내 영혼을 소생시키시고 자기 이름을 위하여 의의 길로 인도하시는도다"],
+        ["이사야 40:31": "오직 여호와를 앙망하는 자는 새 힘을 얻으리니 독수리가 날개치며 올라감 같을 것이요 달음박질하여도 곤비치 아니하겠고 걸어가도 피곤치 아니하리로다"],
+        ["이사야 41:10": "두려워하지 말라 내가 너와 함께 함이니라 놀라지 말라 나는 네 하나님이 됨이니라 내가 너를 굳세게 하리라 참으로 너를 도와 주리라 참으로 나의 의로운 오른손으로 너를 붙들리라"]
     ]
     
     let font: [(fontName: String, displayName: String)] = [("BMYEONSUNG-OTF", "배달의민족 연성체"), ("MangoDdobak-R", "망고보드 또박체"), ("Cafe24Supermagic-OTF-Regular", "카페24슈퍼매직"), ("omyu_pretty", "오뮤다예쁨체"), ("TheJamsilOTF2Light", "롯데 더잠실체"), ("KimjungchulScript-Regular", "김정철 손글씨"), ("KCC-Chassam", "KCC 차쌤체"), ("Dovemayo_wild", "거친둘기마요"), ("AdultKid", "어른아이"), ("Sagaksagak", "사각사각"), ("GamtanRoad-Batang-Regular", "감탄로드 바탕체"), ("seolleimcoolot-SemiBold", "설레임체"), ("SKYBORI", "하늘보리체"), ("RIDIBatang", "리디 바탕체"), ("Ownglyph_eunbyul21-Rg", "온글잎 은별"), ("Ownglyph_ryuttung-Rg", "온글잎 류뚱체"), ("Ownglyph_noocar-Rg", "온글잎 누카"), ("Ownglyph_jooreeletter-Rg", "온글잎 주리손편지"), ("KyoboHandwriting2023wsa", "교보 손글씨 우선아"), ("Yeongdeok Blueroad", "영덕 블루로드체"), ("Yeongdeok Sea", "영덕 바다체"), ("SUITE-Regular", "스위트"), ("NanumSquareNeo-bRg", "나눔스퀘어 네오"), ("NanumBarunpenOTF", "나눔 바른펜"), ("NanumHimNaeRaNeunMarBoDan", "나눔 힘내라는말"), ("NanumYaGeunHaNeunGimJuIm", "나눔 김주임"), ("LINESeedSansKR-Regular", "Line Seed"), ("GangwonEduAll-OTFLight", "강원교육 모두체"), ("Pretendard-Regular", "프리텐다드")]
